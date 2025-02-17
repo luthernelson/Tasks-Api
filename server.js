467cdd3243af3a1065f411dafe3d8311314e80a2
Luther Nelson <luthernelson732@gmail.com>
2025-02-17 12:31:32 +0100

    version3

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ app.use('/api', getUser);
 app.use('/api', getSharedTasks);
 app.use('/api', getCommentByTask);
 
-const port = 3000;
+const port =process.env.PORT || 4000;
 
 io.on('connection', (socket) => {
   console.log('Un utilisateur est connecté');
